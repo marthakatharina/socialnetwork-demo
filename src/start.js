7a6573664b915e3be550fcaf36b808ad4693f514
Marta Wlusek <wlusek.marta@gmail.com>
2020-11-03 22:52:10 +0100

    logo component added

--- src/start.js
+++ src/start.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import Registration from "./registration";
 import Welcome from "./welcome";
+import Logo from "./logo";
 
 let elem;
 const userIsLoggedIn = location.pathname != "/welcome";
@@ -9,7 +10,8 @@ const userIsLoggedIn = location.pathname != "/welcome";
 if (!userIsLoggedIn) {
     elem = <Welcome />;
 } else {
-    elem = <h1>I will be the logo component</h1>;
+    // elem = <h1>I will be the logo component</h1>;
+    elem = <Logo />;
 }
 
 ReactDOM.render(/*<Registration/>*/ elem, document.querySelector("main"));
